750b9361e496b506a02316b70cb7a148f4d9befc
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2023-09-07 08:46:09 +0700

    change page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Login from "./pages/Login";
+// import Register from "./pages/Register";
+
 function App() {
     return (
-        <div className="App">
-            <h1 className="text-3xl font-bold underline">Hello World!</h1>
-        </div>
+        <Login />
     );
 }
 

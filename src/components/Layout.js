12bea66fa0130446063fb57bf2e2b03451821340
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2023-09-07 23:07:23 +0700

    finish layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,18 +1,38 @@
 import { Outlet } from "react-router-dom"
-import { logOut } from "../services/auth/authSlice"
-import { useDispatch } from "react-redux"
 
+import Logo from '../assets/logo/Logo.png'
 
 const Layout = () => {
-    const dispatch = useDispatch()
-
-    const handleLogout = () => dispatch(logOut())
-
     return (
-        <div>
-            <button onClick={handleLogout}>Keluar</button>
-            <Outlet />
-        </div>
+        <>
+            <div className="fixed border border-1 w-full mt-[54px]"></div>
+            <div className="px-[80px]">
+                <nav className="min-w-full py-3 text-white flex justify-between items-center">
+                    <div className='flex'>
+                        <img src={Logo} alt='Logo' width={30} />
+                        <h1 className='font-bold text-[20px] ml-2 text-[#2E2E2E]'>SIMS PPOB</h1>
+                    </div>
+                    <ul className='grid grid-cols-3 gap-10 list-none justify-items-center'>
+                        <li className='font-bold' >
+                            <h1 className='hover:cursor-pointer text-[#2E2E2E]'>
+                                Top Up
+                            </h1>
+                        </li>
+                        <li className='font-bold' >
+                            <h1 className='hover:cursor-pointer text-[#2E2E2E]'>
+                                Transaction
+                            </h1>
+                        </li>
+                        <li className='font-bold' >
+                            <h1 className='hover:cursor-pointer text-[#2E2E2E]'>
+                                Akun
+                            </h1>
+                        </li>
+                    </ul>
+                </nav>
+                <Outlet />
+            </div>
+        </>
     )
 }
 

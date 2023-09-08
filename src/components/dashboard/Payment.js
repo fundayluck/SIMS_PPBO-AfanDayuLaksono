dd1f53b05ca2b4db67567bfcfb3d50a6c15e7efd
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2023-09-09 02:58:06 +0700

    finish payment

--- src/components/dashboard/Payment.js
+++ src/components/dashboard/Payment.js
@@ -1,26 +1,91 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import CurrencyInput from 'react-currency-input-field'
 import { selectCurrentToken } from '../../services/auth/authSlice'
 import { useSelector } from 'react-redux'
 import { useLazyGetServicesQuery } from '../../services/modules/information'
 import { PiMoneyFill } from 'react-icons/pi'
 import Button from '../common/Button'
+import { useLazyDoPaymentQuery } from '../../services/modules/transaction'
+import { ImSpinner2 } from 'react-icons/im'
+import Swal from 'sweetalert2'
 
 const Payment = () => {
     const token = useSelector(selectCurrentToken)
     const [getServices] = useLazyGetServicesQuery()
+    const [doPayment, { isFetching }] = useLazyDoPaymentQuery()
     const [getService, setGetService] = useState([])
-
+    const [tarif, setTarif] = useState('')
+    const navigate = useNavigate()
     const { id } = useParams()
+
+    const bilangan = tarif
+
+    let number_string = bilangan.toString(),
+        sisa = number_string.length % 3,
+        rupiah = number_string.substr(0, sisa),
+        ribuan = number_string.substr(sisa).match(/\d{3}/g);
+
+    if (ribuan) {
+        let separator = sisa ? '.' : '';
+        rupiah += separator + ribuan.join('.');
+    }
 
     useEffect(() => {
         const getSrvcs = async () => {
             const res = await getServices(token)
-            setGetService(res.data.data[id]);
+            setGetService(res?.data?.data[id]);
+            setTarif(res?.data?.data[id]?.service_tariff)
         }
         getSrvcs()
     }, [getServices, token, id])
+
+    const processPayment = async (e) => {
+        e.preventDefault()
+        const data = { service_code: getService?.service_code }
+        try {
+            const response = await doPayment(data, token)
+            if (response.status === 'fulfilled') {
+                Swal.fire(
+                    'Yeay!',
+                    `Top Up sebesar <b>${rupiah}</b> berhasil!`,
+                    'success'
+                )
+                navigate('/dashboard')
+            } else if (response.status === "rejected") {
+                Swal.fire(
+                    'Gagal!',
+                    `${response.error.data.message}`,
+                    'error'
+                )
+                navigate('/dashboard')
+            }
+        } catch (error) {
+            console.log(error);
+            Swal.fire(
+                'Error!',
+                `${error.message}`,
+                'error'
+            )
+            navigate('/dashboard')
+        }
+    }
+
+    const handlePayment = (e) => {
+        Swal.fire({
+            title: `Beli ${getService.service_name} senilai`,
+            html: `<b>Rp.${rupiah}?</B> `,
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '',
+            confirmButtonText: 'Ya, lanjutkan Bayar!',
+            cancelButtonText: 'Batalkan'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                processPayment(e)
+            }
+        })
+    }
 
     return (
         <div className='mt-5'>
@@ -45,8 +110,8 @@ const Payment = () => {
                 groupSeparator="."
             />
             <Button
-                // onClick={DoTopup}
-                buttonName='Top Up'
+                onClick={handlePayment}
+                buttonName={isFetching ? <ImSpinner2 className='animate-spin text-[22px] text-white' /> : 'Bayar'}
                 colorButton={'bg-[#F42619] hover:opacity-80'}
                 styleButton='w-full h-[32px] mt-2 text-white font-bold rounded shadow-md'
             />
--- src/services/modules/transaction/index.js
+++ src/services/modules/transaction/index.js
@@ -1,4 +1,5 @@
 import { api } from '../../api'
+import doPayment from './doPayment'
 import doTopup from './doTopup'
 import getBalance from './getBalance'
 
@@ -6,9 +7,14 @@ import getBalance from './getBalance'
 export const transactionApi = api.injectEndpoints({
     endpoints: build => ({
         getBalance: getBalance(build),
-        doTopup: doTopup(build)
+        doTopup: doTopup(build),
+        doPayment: doPayment(build)
     }),
     overrideExisting: true,
 })
 
-export const { useLazyGetBalanceQuery, useLazyDoTopupQuery } = transactionApi
+export const {
+    useLazyGetBalanceQuery,
+    useLazyDoTopupQuery,
+    useLazyDoPaymentQuery
+} = transactionApi
